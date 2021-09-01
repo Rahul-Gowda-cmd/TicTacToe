0677b53c193c4e43ef07eb211b98da9a4311bd9b
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-09-01 12:05:27 +0530

    feat:Displaying Current Board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -9,21 +9,16 @@ namespace TicTacToe
             string computerChoose;
             Console.WriteLine("Plaese enter Your Letter(X/O) : ");
             string userChoose = Console.ReadLine();
-            while (true)
+            if (userChoose == "X" || userChoose == "x")
             {
-                if (userChoose == "X" || userChoose == "x")
-                {
-                    computerChoose = "O";
-                    break;
-                }
-                else if (userChoose == "O" || userChoose == "o")
-                {
-                    computerChoose = "X";
-                    break;
-                }
-                else
-                    Console.WriteLine("Wrong input,Please type again");
+                computerChoose = "O";
             }
+            else if (userChoose == "O" || userChoose == "o")
+            {
+                computerChoose = "X";
+            }
+            else
+                Console.WriteLine("Wrong input,Please type again");
         }
         static void CreateEmptyBoard()
         {
@@ -32,6 +27,15 @@ namespace TicTacToe
             {
                 board[i] = ' ';
             }
+            Console.WriteLine("    |      |    ");
+            Console.WriteLine(" {0}  |  {1}   |  {2} ", board[1], board[2], board[3]);
+            Console.WriteLine("____|______|____");
+            Console.WriteLine("    |      |    ");
+            Console.WriteLine(" {0}  |  {1}   |   {2}  ", board[4], board[5], board[6]);
+            Console.WriteLine("____|______|____");
+            Console.WriteLine("    |      |    ");
+            Console.WriteLine(" {0}  |  {1}   |   {2}  ", board[7], board[8], board[9]);
+            Console.WriteLine("____|______|____");
         }
         static void Main(string[] args)
         {
